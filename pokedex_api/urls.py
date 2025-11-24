2c37a08549e64cada052168b4ff337e41e7ea244
Lívia <liviagabriele.fs@gmail.com>
2025-11-24 12:02:01 -0300

    urls

--- pokedex_api/urls.py
+++ pokedex_api/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
+
+# pokedex_api/urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Define que todas as URLs do app 'api' começarão com 'api/'
+    # Ex: http://localhost:8000/api/treinadores/
+    path('api/', include('api.urls')),
 ]
